79b1918161bc270c63babaf44ddc2f62069f14a7
ColinXHL <alin989877@gmail.com>
2026-03-30 22:32:56 +0800

    ci(cnb): add web trigger button config
    Add CNB web trigger button mapping to web_trigger_one and remove legacy file header text from cnb_trigger.py to avoid Python escape-sequence warnings in Actions logs.

--- .github/workflows/cnb_trigger.py
+++ .github/workflows/cnb_trigger.py
@@ -1,11 +1,3 @@
-'''
-Author: ColinXHL alin989877@gmail.com
-Date: 2026-03-30 21:19:54
-LastEditors: ColinXHL alin989877@gmail.com
-LastEditTime: 2026-03-30 21:31:43
-FilePath: \AkashaNavigator\.github\workflows\cnb_trigger.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
